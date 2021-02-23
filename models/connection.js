6ec5660aa83fd29ecf7f728116a75aa17f76b1c7
Renato Filho <renatosousafilho@gmail.com>
2021-02-23 12:43:29 -0300

    evalauator true

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,8 @@
 const mongoClient = require('mongodb').MongoClient;
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const evalautor  = true;
+
+const MONGO_DB_URL = (evalautor) ?  'mongodb://mongodb:27017/StoreManager' : 'mongodb://localhost:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 const connection = () =>
